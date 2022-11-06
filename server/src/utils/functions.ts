4f152ab9226138e2cf8bf338099f601f493decfc
MohamedOMourad <mohamed.o.murad@gmail.com>
2022-11-06 20:24:00 +0200

    change file system path

--- server/src/utils/functions.ts
+++ server/src/utils/functions.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export const getTestData = () => {
-    const filePath = path.join(process.cwd(), 'src', 'resources', 'TestData.json');
+    const filePath = path.join(process.cwd(), 'src', 'data', 'TestData.json');
     const fileData = fs.readFileSync(filePath)
     const testData = JSON.parse(fileData.toString())
     return testData
